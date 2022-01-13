4a6e5fa9e9981617c511d99f3b4a39541cabe99a
bit-ranger <sincerebravefight@gmail.com>
2022-01-13 19:08:31 +0800

    feat(setting): setting works

--- preload.js
+++ preload.js
@@ -1,28 +1,151 @@
-// const path = require('path')
-// const fs = require('fs')
+const fs = require('fs')
+const electron = require('electron')
+const child_process = require("child_process")
+const os = require('os');
+const path = require("path")
+
+ctlKey = utools.isMacOs() ? 'command' : 'control'
+
+
+let getSleepCodeByShell = ms => {
+    var cmd, tempFilePath
+    if (utools.isWindows()) {
+        tempFilePath = getQuickcommandTempFile('vbs')
+        cmd = `echo set ws=CreateObject("Wscript.Shell") > ${tempFilePath} && echo Wscript.sleep ${ms} >> ${tempFilePath} && cscript /nologo ${tempFilePath}`
+    } else {
+        cmd = `sleep ${ms / 1000}`
+    }
+    return cmd
+}
+
+
+getQuickcommandTempFile = ext => {
+    return path.join(os.tmpdir(), `quickcommandTempFile.${ext}`)
+}
+
+
+
+let func = {
+    readClip: () => {
+        return electron.clipboard.readText();
+    },
+
+    writeClip: (text) => {
+        electron.clipboard.writeText(text);
+    },
+
+    // 模拟粘贴操作
+    simulatePaste: () => {
+        utools.simulateKeyboardTap('v', ctlKey);
+    },
+
+    includesAll: (raw, kws) => {
+        for (let k of kws) {
+            if (!raw.includes(k)) {
+                return false;
+            }
+        }
+        return true;
+    },
+
+    sleep: (ms) => {
+        var start = new Date().getTime()
+        try {
+            // node 16.13.1
+            child_process.execSync(getSleepCodeByShell(ms), {timeout: ms, windowsHide: true})
+        } catch (ex) {
+        }
+        var end = new Date().getTime()
+        return (end - start)
+    },
+}
 
 let snippetDataCache = []
-let tabListCache = []
+
 
 window.exports = {
-    'search_snippet': {
+    'snippet_search': {
         mode: 'list',
         args: {
             enter: (action, callbackSetList) => {
-                snippetDataCache = []
+                let mdPath = utools.dbStorage.getItem('markdown_path');
+                if (!mdPath) {
+                    callbackSetList([{
+                        title: "please set markdown path"
+                    }])
+                    return;
+                }
+
+                let raw = "";
+                try {
+                    raw = fs.readFileSync(mdPath, 'utf8');
+                } catch (e) {
+                    callbackSetList([{
+                        title: "invalid markdown path",
+                        description: mdPath
+                    }])
+                    return;
+                }
+                let blocks = raw.split(/#\s/)
+                    .filter(i => i.trim() !== "");
+
+                snippetDataCache = blocks.map(b => {
+                    let rows = b.split(/\n/).filter(r => !r.startsWith("```"));
+                    return {
+                        title: rows[0],
+                        description: rows.slice(1).join("\n")
+                    }
+                });
+
+                callbackSetList(snippetDataCache)
             },
             search: (action, searchWord, callbackSetList) => {
-                if (!searchWord) return callbackSetList()
+                if (!searchWord) {
+                    return callbackSetList(snippetDataCache)
+                }
                 searchWord = searchWord.toLowerCase()
-                return callbackSetList(snippetDataCache.filter(x => x.lowTitle.includes(searchWord)))
+                let kws = searchWord.split(/\s/).filter(i => i.trim() !== "");
+                return callbackSetList(snippetDataCache.filter(item => func.includesAll(item.title, kws)))
             },
-            select: (action, itemData) => {
-                window.utools.hideMainWindow()
-                shell.openExternal(itemData.description).then(() => {
-                    window.utools.outPlugin()
-                })
+            select: (action, item) => {
+                try {
+                    let oldClip = func.readClip();
+                    func.writeClip(item.description);
+                    utools.hideMainWindow()
+                    func.simulatePaste();
+                    func.sleep(300)
+                    func.writeClip(oldClip);
+                } catch (e) {
+                    utools.showNotification(JSON.stringify(e))
+                }
+
             },
-            placeholder: "搜索"
+            placeholder: "Search"
+        }
+    },
+    'snippet_setting': {
+        mode: 'none',
+        args: {
+            enter: (action) => {
+                let subInput = "";
+                utools.setSubInput(({text}) => {
+                    subInput = text;
+                }, "markdown path");
+
+                document.addEventListener('keydown', event => {
+                    if (event.keyCode === 13) {
+                        utools.dbStorage.setItem('markdown_path', subInput);
+                        utools.hideMainWindow()
+                    }
+                });
+
+            },
         }
     }
 }
+
+
+
+
+
+
